f6b7a1f6ae29ca85e7105cc0f801fbce41423f8e
Ananna14 <https://github.com/Ananna14>
2022-01-01 21:06:32 +0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Pages/Nav/Nav';
+// import Home from './Pages/Banner/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Banner/Home/Home';
+import About from './Pages/Banner/Home/About/About';
+import Contact from './Pages/Banner/Home/About/Contact/Contact';
+import Banner from './Pages/Banner/Banner';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+         <Route path="/" element={<Home/>}></Route>
+         <Route path="/home" element={<Home/>}></Route>
+         <Route path="/about" element={<About/>}></Route>
+         <Route path="/contact" element={<Contact/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/Banner/Home/About/SingleAbout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Card } from 'react-bootstrap';
+
+const SingleAbout = ({teach}) => {
+    const {img, name, salary} = teach;
+    return (
+        <div className='col-lg-4 col-md-6 col-12 card-design'>
+        <Card className="card" style={{ width: '18rem' }}>
+<Card.Img variant="top" className='img-design h-70' src={img} />
+<Card.Body>
+    <Card.Title>{name}</Card.Title>
+    <Card.Text>
+   {salary}
+    </Card.Text>
+    {/* <Button variant="primary">Go somewhere</Button> */}
+</Card.Body>
+</Card>
+</div>
+    )
+}
+
+export default SingleAbout
